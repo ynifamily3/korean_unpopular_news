54092b5df53c9aaae1bfe74172096586b53089a1
Andrew <escapeanaemia@gmail.com>
2020-05-09 02:51:41 +0900

    create the ArticleCell_img / Category

--- src/client/Article.js
+++ src/client/Article.js
@@ -4,11 +4,25 @@ import PropTypes from "prop-types";
 function Article({ title, attacedImg, category, body }) {
   return (
     <div className="Article">
-      <div className="ArticleCol" id="ArticleCol2"></div>
+      <div className="ArticleCol" id="ArticleCol2">
+        <ArticleCell poster={attacedImg} alt={title} />
+      </div>
       <div className="ArticleCol">
         <h1>{title}</h1>
-        <div className="ArticleCategory"></div>
+        <div className="ArticleCategory">
+          {category.map((category, index) => (
+            <ArticleCategory category={category} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
+}
+
+function ArticleCell({ poster, alt }) {
+  return <img src={poster} alt={alt} title={alt} className="Article__Poster" />;
+}
+
+function ArticleCategory({ category }) {
+  return <span className="ArticleCategory">{category}</span>;
 }
