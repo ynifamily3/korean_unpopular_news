4ccb0d5836253c47ae68e873a70aa6faf8e9b9b7
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-05-23 15:37:39 +0900

    서버사이드 헬멧 적용

--- src/client/pages/Main.tsx
+++ src/client/pages/Main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Main = (): JSX.Element => {
   return (
     <div>
+      <Helmet>
+        <title>메인페이지!</title>
+      </Helmet>
       <h2>Main페이지입니다</h2>
       <Link to="/about">어바웃페이지 가기</Link>
     </div>
