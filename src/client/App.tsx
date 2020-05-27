1a141e317f3b483ecd6f1192b202bd6635a12fbb
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-05-27 20:22:59 +0900

    Material UI

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import loadable from "@loadable/component";
 import Top from "./components/Top";
+import { Button } from "@material-ui/core";
 
 const Main = loadable(() =>
   import(/* webpackChunkName: "MainPage" */ "./pages/Main")
@@ -14,6 +15,9 @@ const App = (): JSX.Element => {
   return (
     <>
       <Top />
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       <Switch>
         <Route exact path="/" render={(): JSX.Element => <Main />} />
         <Route exact path="/about" render={(): JSX.Element => <About />} />
--- src/server/controllers/ssr.tsx
+++ src/server/controllers/ssr.tsx
@@ -44,8 +44,9 @@ export default function ssr(req, res): void {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <link rel="manifest" href="/site.webmanifest">
-          <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
         </head>
         <body>
           <div id="main">${html}</div>
