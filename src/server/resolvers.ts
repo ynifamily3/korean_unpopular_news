018d1a907627199291badd416028c8945746081b
Jisu Kim <webmaster@alien.moe>
2020-05-31 22:44:15 +0900

    Add offset argument

--- src/server/resolvers.ts
+++ src/server/resolvers.ts
@@ -1,7 +1,7 @@
-import { Resolver, Query, ArgsType, Field, Int, Args } from "type-graphql";
+import { Resolver, Query, ArgsType, Field, Int, Args, ID } from "type-graphql";
 import { NewsArticle } from "../database/entity/NewsArticle";
 import { InjectRepository } from "typeorm-typedi-extensions";
-import { Repository, Between, In } from "typeorm";
+import { Repository, Between, In, MoreThan } from "typeorm";
 import { Min } from "class-validator";
 
 @ArgsType()
@@ -9,6 +9,12 @@ class NewsArticlesArgs {
   @Field(() => Int, { description: "가져올 개수" })
   @Min(1)
   limit = 15;
+
+  @Field(() => ID, {
+    nullable: true,
+    description: "가져올 목록의 바로 이전 ID값",
+  })
+  offset?: string;
 
   @Field({ description: "시작 날짜" })
   start: Date;
@@ -32,14 +38,20 @@ export class NewsResolver {
 
   @Query(() => [NewsArticle])
   async newsArticles(
-    @Args() { limit, start, end, desc, keywords }: NewsArticlesArgs
+    @Args() { limit, start, end, desc, keywords, offset }: NewsArticlesArgs
   ): Promise<NewsArticle[]> {
-    return await this.newsRepo
+    const qb = this.newsRepo
       .createQueryBuilder("news")
       .innerJoinAndSelect("news.keywords", "keywords")
-      .where({ createdAt: Between<Date>(start, end || new Date()) })
-      .orderBy("news.createdAt", desc ? "DESC" : "ASC")
-      .take(limit)
-      .getMany();
+      .where({
+        createdAt: Between<Date>(start, end || new Date()),
+      })
+      .orderBy("news.createdAt", desc ? "DESC" : "ASC");
+
+    if (offset) qb.andWhere("news.id > :offset", { offset });
+
+    qb.take(limit);
+
+    return qb.getMany();
   }
 }
