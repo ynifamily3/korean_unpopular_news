5cb1e85edf5e04cc3156661b2123cc7026363dcb
Andrew <escapeanaemia@gmail.com>
2020-06-02 10:48:14 +0900

    object mapper 추가

---
+++ src/ios/UnderTimes/UnderTimes/Network.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  UnderTimes
+//
+//  Created by Andrew Han on 2020/05/30.
+//  Copyright © 2020 escapeanaemia. All rights reserved.
+//
+
+import Foundation
+
+
+class URLManager {
+    static var baseURL : URL? = URL(string: baseURLStr)
+    static var baseURLStr = ""
+}
