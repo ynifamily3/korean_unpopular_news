72e2e0d3cb7adb0d51063fb673ad2e83d55fc6c7
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-06-09 14:10:43 +0900

    자잘한 버그 수정

--- src/client/components/Top.tsx
+++ src/client/components/Top.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation, useHistory } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
